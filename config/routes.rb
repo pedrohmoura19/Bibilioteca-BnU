18c076cfdc5555f477fdd58fe2f795158ddb8b96
pedrohmoura19 <pedrohmoura19@gmail.com>
2021-03-11 18:58:16 -0300

    Created basic Book model

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root "books#index"
+
   get "/books", to:"books#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
