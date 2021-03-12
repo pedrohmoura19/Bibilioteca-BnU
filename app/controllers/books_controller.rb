2cba21ed07efc831bd891d1fc91b6f0c2a48775e
pedrohmoura19 <pedrohmoura19@gmail.com>
2021-03-12 16:26:59 -0300

    Created destroy and uptade methods to books

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -23,6 +23,28 @@ class BooksController < ApplicationController
     end
   end
 
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    @book = Book.find(params[:id])
+
+    if @book.update(book_params)
+      redirect_to @book
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+
+    redirect_to root_path
+  end
+
+
   private
     def book_params
       params.require(:book).permit(:title, :author, :year)
